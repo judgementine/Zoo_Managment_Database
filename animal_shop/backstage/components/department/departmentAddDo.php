319baa85fb088392731b993338833338aa9af858
judgementine <42780618+judgementine@users.noreply.github.com>
2021-04-17 19:15:38 -0500

    Mieqi php added and yaml updated

---
+++ animal_shop/backstage/components/department/departmentAddDo.php
@@ -0,0 +1,15 @@
+<?php
+
+require '../../conn.php';
+@$AmountPaid = $_POST['AmountPaid'];
+@$TicketNumber = $_POST['TicketNumber'];
+@$ItemsBought = $_POST['ItemsBought'];
+@$ItemPrice = $_POST['ItemPrice'];
+@$ItemName = $_POST['ItemName'];
+@$AmountBought = $_POST['AmountBought'];
+@$EmergencyContact = $_POST['EmergencyContact'];
+@$FinanceEarnings = $_POST['FinanceEarnings'];
+mysqli_query($conn, "insert into department(AmountPaid,TicketNumber,ItemsBought,ItemPrice,ItemName,AmountBought,EmergencyContact,FinanceEarnings) values('$AmountPaid','$TicketNumber','$ItemsBought','$ItemPrice','$ItemName','$AmountBought','$EmergencyContact','$FinanceEarnings')");
+echo "<script>alert('success');window.location.href='editor.html';</script>"
+
+?>
