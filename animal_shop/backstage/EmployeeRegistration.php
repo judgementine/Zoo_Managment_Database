ec87368324acde9e8a18c87ca8cdca9c53875409
eliassir <81395157+eliassir@users.noreply.github.com>
2021-04-21 17:11:16 -0500

    User authentication

--- animal_shop/backstage/EmployeeRegistration.php
+++ animal_shop/backstage/EmployeeRegistration.php
@@ -23,5 +23,27 @@ $homephone = "";
 $salary = "";
 
 
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $fname = $_POST["fname"];
+    $minit = $_POST["mInit"];
+    $lname = $_POST["lname"];
+    $employeeID = $_POST["employeeID"];
+    $jobTitle = $_POST["jobTitle"];
+    $PhoneNumber = $_POST["PhoneNumber"];
+    $cellphone = $_POST["cellPhone"];
+    $homephone = $_POST["Homephone"];
+    $salary = $_POST["Salary"];
+
+    $sql = "INSERT INTO Employee (ID_Number, Job_Title, Salary, Phone_Number, cell_phone, home_phone, name, first_name, last_name, middle_initial) 
+        VALUES ('$employeeID', '$jobTitle', '$salary', '$PhoneNumber', '$cellphone', '$homephone', '$name', '$fname', '$lname', '$minit')";
+
+    if (mysqli_query($server, $sql) {
+        echo "New record created succefully";
+    } else {
+        echo "error found";
+    }
 }
+
 ?>
